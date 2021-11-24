fccf12e06b7fbf4617eae57dab660403572456c2
Nazim Nasser <nazimnasser94@gmail.com>
2021-11-24 18:53:04 +0200

    js basics 1 step_7

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,8 @@
+var num_1 = window.prompt("Please enter your shoe size");
+var num_2 = window.prompt("Please enter your birthday");
+var result;
+function multiple(){
+    result = ((num_1 * 2) + 5)* 50 - num_2 + 1766
+    alert("the result is: " + result)
+};
+multiple();
