9ddcf1d00e5641bd4c59aa6f594153c2aa9c0226
Nazim Nasser <nazimnasser94@gmail.com>
2021-11-24 18:28:36 +0200

    js basics 1 step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,10 @@
+var name = window.prompt("Please enter your name");
+var surname = window.prompt("Please enter your surname");
+var city = window.prompt("Please enter your city");
+alert(
+    `
+    Nom : ${name}
+    Prenom : ${surname}
+    Ville : ${city}
+    `
+)
