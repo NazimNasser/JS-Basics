0cc67ee591ee59be11d9c0ffb3ef3c41fc0066da
Nazim Nasser <nazimnasser94@gmail.com>
2021-11-25 22:11:57 +0200

    js basics 1 edit step 8

--- javascript-basics-01/step_8.js
+++ javascript-basics-01/step_8.js
@@ -1,7 +1,12 @@
-var age = window.prompt("Please enter your age");
+var currentButton = document.querySelector("button");
 
-if (age > 18){
-    alert("you are over 18")
-} else {
-    alert("you are under 18")
+currentButton.onclick = function(){
+
+    var age = document.querySelector("#age").value;
+
+    if (age > 18){
+        alert("you are over 18")
+    } else {
+        alert("you are under 18")
+    };
 };
