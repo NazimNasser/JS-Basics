a927c67abe3da4d985a66eb2d96ddc2af69233da
Nazim Nasser <nazimnasser94@gmail.com>
2021-11-25 22:01:00 +0200

    js basics 1 edit step 4

--- javascript-basics-01/step_4.js
+++ javascript-basics-01/step_4.js
@@ -1,10 +1,16 @@
-var name = window.prompt("Please enter your name");
-var surname = window.prompt("Please enter your surname");
-var city = window.prompt("Please enter your city");
-alert(
-    `
-    Nom : ${name}
-    Prenom : ${surname}
-    Ville : ${city}
-    `
-)
+var currentButton = document.querySelector("button");
+
+currentButton.onclick = function(){
+
+    var theName = document.querySelector("#name").value,
+    surname = document.querySelector("#surname").value,
+    city = document.querySelector("#city").value;
+
+    alert(
+        `
+        Name : ${theName}
+        surname : ${surname}
+        city : ${city}
+        `
+    );
+};
